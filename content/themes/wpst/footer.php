550e02923ed334fe5427f2c1feaada5e8a685352
Emily Cressey <hello@emily.vc>
2016-12-19 19:23:50 +0000

    change fb link

--- content/themes/wpst/footer.php
+++ content/themes/wpst/footer.php
@@ -1,8 +1,8 @@
 <footer <?php echo !is_front_page() ? ' ' : 'class="footer--main"'; ?>>
     <div class="container <?php echo !is_front_page() ? 'container--small' : ''; ?>">
         <ul class="list--unset | footer__icons">
-            <a href="https://www.instagram.com/jakefogarty/"><li class="icon icon--medium icon--instagram"></li></a>
-            <a href="https://www.facebook.com/jake.fogarty2"><li class="icon icon--medium icon--facebook"></li></a>
+            <a href="https://www.instagram.com/jakefogarty"><li class="icon icon--medium icon--instagram"></li></a>
+            <a href="https://www.facebook.com/JFogartydrumtuition"><li class="icon icon--medium icon--facebook"></li></a>
             <a href="https://www.youtube.com/channel/UClGiew31aRQYC3CDNg5wnOQ"><li class="icon icon--medium icon--youtube"></li></a>
         </ul>
         <a href="mailto:fogartydrumsofficial@gmail.com" class="footer__contact">
