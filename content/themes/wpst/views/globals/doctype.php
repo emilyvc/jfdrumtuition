f762fb494ccf7b04f512732efd59c37dfb3c9874
Emily Cressey <hello@emily.vc>
2016-12-19 21:06:19 +0000

    sort out title

--- content/themes/wpst/views/globals/doctype.php
+++ content/themes/wpst/views/globals/doctype.php
@@ -25,7 +25,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Titles/Descriptions -->
-    <title><?php wp_title( '', true, 'right' ); ?><?php echo ! is_front_page() ? '| ' . bloginfo( 'name' ) : ''; ?></title>
+    <title><?php wp_title( '', true, 'right' ); ?></title>
 
     <!-- Favicons -->
     <?php get_template_part( 'views/globals/favicons' ); ?>
